7565192452be659daacd16503fa46c13561637a7
sketchbooks99 <kiuchsketch@ezweb.ne.jp>
2019-07-16 15:07:49 +0900

    error in shader compile

--- src/script.js
+++ src/script.js
@@ -12,23 +12,34 @@ onload = function() {
 
     var attLocation = new Array();
     attLocation[0] = gl.getAttribLocation(prg, 'position');
-    attLocation[1] = gl.getAttribLocation(prg, 'color');
+    attLocation[1] = gl.getAttribLocation(prg, 'normal');
+    attLocation[2] = gl.getAttribLocation(prg, 'color');
 
     var attStride = new Array();
     attStride[0] = 3;
-    attStride[1] = 4;
+    attStride[1] = 3;
+    attStride[2] = 4;
 
     var torus_data = torus(32, 32, 1.0, 2.0);
-    var pos_vbo = create_vbo(torus_data[0]);
-    var col_vbo = create_vbo(torus_data[1]);
+    var position = torus_data[0];
+    var normal = torus_data[1];
+    var color = torus_data[2];
+    var index = torus_data[3];
 
-    set_attribute([pos_vbo, col_vbo], attLocation, attStride);
+    var pos_vbo = create_vbo(position);
+    var nor_vbo = create_vbo(normal);
+    var col_vbo = create_vbo(color);
 
-    var ibo = create_ibo(torus_data[2]);
+    set_attribute([pos_vbo, nor_vbo, col_vbo], attLocation, attStride);
+
+    var ibo = create_ibo(index);
 
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
 
-    var uniLocation = gl.getUniformLocation(prg, 'mvpMatrix');
+    var uniLocation = new Array();
+    uniLocation[0] = gl.getUniformLocation(prg, 'mvpMatrix');
+    uniLocation[1] = gl.getUniformLocation(prg, 'invMatrix');
+    uniLocation[2] = gl.getUniformLocation(prg, 'lightDirection');
 
     // minMatrix.jsを用いた行列関連処理
     var m = new matIV();
@@ -38,10 +49,13 @@ onload = function() {
     var pMatrix = m.identity(m.create());
     var tmpMatrix = m.identity(m.create());
     var mvpMatrix = m.identity(m.create());
+    var invMatrix = m.identity(m.create());
 
     m.lookAt([0.0, 0.0, 20.0], [0, 0, 0], [0, 1, 0], vMatrix);
     m.perspective(45, c.width / c.height, 0.1, 100, pMatrix);
     m.multiply(pMatrix, vMatrix, tmpMatrix);
+
+    var lightDirection = [-0.5, 0.5, 0.5];
 
     var count = 0;
 
@@ -62,9 +76,13 @@ onload = function() {
         m.identity(mMatrix);
         m.rotate(mMatrix, rad, [0, 1, 0.8], mMatrix);
         m.multiply(tmpMatrix, mMatrix, mvpMatrix);
-        gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
 
-        gl.drawElements(gl.TRIANGLES, torus_data[2].length, gl.UNSIGNED_SHORT, 0);
+        m.inverse(mMatrix, invMatrix);
+        gl.uniformMatrix4fv(uniLocation[0], false, mvpMatrix);
+        gl.uniformMatrix4fv(uniLocation[1], false, invMatrix);
+        gl.uniform3fv(uniLocation[2], lightDirection);
+
+        gl.drawElements(gl.TRIANGLES, index.length, gl.UNSIGNED_SHORT, 0);
 
         gl.flush();
 
@@ -150,18 +168,21 @@ onload = function() {
     }
 
     function torus(row, column, irad, orad) {
-        var pos = new Array(), col = new Array(), idx = new Array();
+        var pos = new Array(), nor = new Array(), 
+            col = new Array(), idx = new Array();
         for(var i=0; i<=row; i++) {
             var r = Math.PI * 2 / row * i;
             var rr = Math.cos(r);
             var ry = Math.sin(r);
-
             for(var j = 0; j <= column; j++) {
                 var tr = Math.PI * 2 / column * j;
                 var tx = (rr * irad + orad) * Math.cos(tr);
                 var ty = ry * irad;
                 var tz = (rr * irad + orad) * Math.sin(tr);
+                var rx = rr * Math.cos(tr);
+                var rz = rr * Math.sin(tr);
                 pos.push(tx, ty, tz);
+                nor.push(rx, ry, rz);
                 var tc = hsva(360 / column * j, 1, 1, 1);
                 col.push(tc[0], tc[1], tc[2], tc[3]);
             }
@@ -173,7 +194,7 @@ onload = function() {
                 idx.push(r + column + 1, r + column + 2, r + 1);
             }
         }
-        return [pos, col, idx];
+        return [pos, nor, col, idx];
     }
 
     function hsva(h, s, v, a) {
